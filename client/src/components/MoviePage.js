c7821a5f395d31be437c6bd073b19bab46e6202c
HarikaGutha <61112886+HarikaGutha@users.noreply.github.com>
2020-10-01 13:35:04 +0100

    Update MoviePage.js
    written code to display content on the screen.

--- client/src/components/MoviePage.js
+++ client/src/components/MoviePage.js
@@ -41,3 +41,61 @@ function parseToJson(response) {
   
   return(movielist);
 }
+
+export default class MoviePage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = { 
+      movies: [] 
+    };
+  }
+
+  componentDidMount() {
+    this.MovieList();
+  }
+
+  MovieList(){
+    if (HOST === null && window.location.host.indexOf('play') > 0) {
+      REQ_URL = "http://" + window.location.host.replace("-80", "-8080")+"/movieplex7/webresources/movie/";
+    };
+    
+    return (
+      fetch(REQ_URL, {mode: 'cors'})
+      .then(status)
+      .then(clone)
+      .then(text)
+      .then(parseToJson)
+      .then((movies) => {this.setState({ movies })
+      })
+    )
+  } 
+
+    const { movies } = this.state;
+    const id = this.props.params.id;
+    const movie = movies.filter((movie) => movie.id == id)[0]
+    if (!movie) {
+      return <NotFoundPage/>;
+    }
+    const headerStyle = { backgroundImage: `url(/img/banner.jpg)` };
+    return (
+      <div className="movie-full">        
+        <MoviesMenu movies={movies}/>
+        <div className="movie">
+          <header style={headerStyle}/>
+          <div className="picture-container">
+            <img src={`/img/${movie.poster}`}/>
+            <h2 className="name">{movie.name}</h2>
+          </div>
+          <section className="description">
+            Cast: {movie.actors} <br />
+            Rating: {movie.rating} <br />
+            (Find out more on <a href={movie.link} target="_blank">IMDB</a>).
+          </section>
+        </div>
+        <div className="navigateBack">
+          <Link to="/">« Back to the index page</Link>
+        </div>
+      </div>
+    );
+  }
+}
