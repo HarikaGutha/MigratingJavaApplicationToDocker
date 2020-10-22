78c42fdd8f9146489fdffb3922836997d9cb8e69
HarikaGutha <61112886+HarikaGutha@users.noreply.github.com>
2020-10-22 18:26:48 +0100

    Update Theater.java

--- movieplex7/src/main/java/org/javaee7/movieplex7/entities/Theater.java
+++ movieplex7/src/main/java/org/javaee7/movieplex7/entities/Theater.java
@@ -52,9 +52,6 @@ import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
-/**
- * @author Arun Gupta
- */
 @Entity
 @Table(name = "THEATER")
 @XmlRootElement
