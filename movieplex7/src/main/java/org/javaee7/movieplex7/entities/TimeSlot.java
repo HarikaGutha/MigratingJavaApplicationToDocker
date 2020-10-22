4aa0be417cbaa6aec48212232a4b5cc4e0f3c78a
HarikaGutha <61112886+HarikaGutha@users.noreply.github.com>
2020-10-22 18:27:08 +0100

    Update TimeSlot.java

--- movieplex7/src/main/java/org/javaee7/movieplex7/entities/TimeSlot.java
+++ movieplex7/src/main/java/org/javaee7/movieplex7/entities/TimeSlot.java
@@ -54,9 +54,6 @@ import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
-/**
- * @author Arun Gupta
- */
 @Entity
 @Table(name = "TIMESLOT")
 @XmlRootElement
