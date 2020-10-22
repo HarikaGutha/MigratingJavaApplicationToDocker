6df66aa2237254e14b7416ac3b526ed394327d2f
HarikaGutha <61112886+HarikaGutha@users.noreply.github.com>
2020-10-22 18:25:57 +0100

    Update Movie.java

--- movieplex7/src/main/java/org/javaee7/movieplex7/entities/Movie.java
+++ movieplex7/src/main/java/org/javaee7/movieplex7/entities/Movie.java
@@ -53,9 +53,6 @@ import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
-/**
- * @author Arun Gupta
- */
 @Entity
 @Table(name = "MOVIE")
 @XmlRootElement
